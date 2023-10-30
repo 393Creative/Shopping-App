f35d94df1533d2ee92e8396d611cbe2ef19228ef
393Creative <143082004+393Creative@users.noreply.github.com>
2023-10-30 13:28:35 +0800

    commit 1

---
+++ index.js
@@ -0,0 +1,7 @@
+const addBtn = document.getElementById("add-button")
+const inputEl = document.getElementById("input-field")
+
+addBtn.addEventListener("click", function() {
+    let inputValue = inputEl.value
+    console.log(inputValue)
+})
