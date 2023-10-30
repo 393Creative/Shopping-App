d22ce22c26a1f7c50518715b914ebf83087573ec
393Creative <143082004+393Creative@users.noreply.github.com>
2023-10-30 18:00:48 +0800

    Commit 4

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const appSettings = {
     databaseURL: "https://playground-c1191-default-rtdb.firebaseio.com/"
 }
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push()} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
-
-
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
-
-
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingList = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
-    push(shoppingListInDB, inputValue+)
-    
+    push(shoppingListInDB, inputValue)
+    clearInput() 
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.entries(snapshot.val())
+    
+    shoppingListClear()
+    
+    for (let i = 0; i < itemsArray.length; i++) {
+        let currentItem = itemsArray[i]
+
+        let currentItemID = currentItem[0]
+        let currentItemValue = currentItem[1]
+        
+        appendItemToShoppingListEl(currentItem)
+    }
+})
+
+function shoppingListClear() {
+    shoppingList.innerHTML = ""
+}
+
+function clearInput() {
+    inputFieldEl.value = ""
+    
+}
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function() {
+        let exactLocationOfListItem = ref(database, `shoppingList/${itemID}`)
+
+        this.remove(exactLocationOfListItem)
+    })
+
+    shoppingList.append(newEl)
+}
